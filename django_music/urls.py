8811b2ba845a929304d0b90d6076563b16f59e08
Thomas Morris <tomchrismorris@yahoo.com>
2022-08-22 17:44:46 -0400

    set up urls in albums

--- django_music/urls.py
+++ django_music/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('__debug__/', include('debug_toolbar.urls')),
 ]
+
+
+urlpatterns += [
+    path('albums/', include('albums.urls')),
+]
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='albums/', permanent=True)),
+]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
