e2c240799cb05cbc715d304e55f608e44f431825
Thomas Morris <tomchrismorris@yahoo.com>
2022-08-22 18:58:35 -0400

    updated initial models Album and Artist

--- albums/models.py
+++ albums/models.py
@@ -4,26 +4,19 @@ from django.urls import reverse
 
 class Album(models.Model):
     title = models.CharField(max_length=200, help_text='Enter a album')
-    artist = models.ManyToManyField('Artist', on_delete=models.SET_NULL, null=True)
+    artist = models.ForeignKey('Artist', on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['-title', '-artist', '-created_at']
-
-    def get_absolute_url(self):
-        return reverse('album-detail-view', args=[str(self.id)])
 
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('album-detail-view', args=[str(self.id)])
+
 
 class Artist(models.Model):
-    name = models.CharField(max_length=200, help_text='Enter a artist')
-    album = models.ManyToManyField('Album', on_delete=models.SET_NULL, null=True)
-
-    class Meta:
-        ordering = ['-name', '-album']
-
+    name = models.CharField(max_length=200, help_text='Enter a band or artist')
+    
     def get_absolute_url(self):
         return reverse('artist-detail-view', args=[str(self.id)])
 
